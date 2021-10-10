eea9be8204def2b916ed1fb6fbfb794c1c5728f3
user <user@DESKTOP-FLHNA44.cg.shawcable.net>
2021-10-10 02:23:15 -0600

    doPost and editnote are finished. Cannot reset title and contents when doing clean and build.

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -1,9 +1,12 @@
 package servlets;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,32 +14,48 @@ import javax.servlet.http.HttpServletResponse;
 import models.Note;
 
 public class NoteServlet extends HttpServlet {
-
+    
+    Note note = new Note();
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-       
+        
         String path = getServletContext().getRealPath("/WEB-INF/note.txt");
         BufferedReader br = new BufferedReader(new FileReader(new File(path)));
         String title = br.readLine();
         String contents = br.readLine();
-
-        Note note = new Note(title, contents);
+        
+        note.setTitle(title);
+        note.setContents(contents);
         request.setAttribute("note", note);
-
+        
+        
         if (request.getParameter("edit") != null) {
             getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
             return;
         }
-
+        
         getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
         return;
     }
-
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        String title = request.getParameter("title");
+        String contents = request.getParameter("contents");
+        note.setTitle(title);
+        note.setContents(contents);
+        request.setAttribute("note", note);
+        
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false)));
+        pw.flush();
+        pw.write(title + "\n" + contents);
+        pw.close();
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+        return;
     }
 }
